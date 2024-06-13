da8c7bdb8c28532b6a752f3b8d331ee5f8a52343
LouisGardet <louisegardet@gmail.com>
2024-06-13 02:42:01 +0200

    Appel api calendar
    Les event s'affichent sur le calendrier

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -10,16 +10,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class EventController extends AbstractController
 {
     #[Route('/event', name: 'app_event')]
     public function index(EventRepository $eventRepository): Response
     {
-        $events = $eventRepository->findAll();
-
         return $this->render('event/index.html.twig', [
-            'events' => $events,
+            'events' => $eventRepository->findAll(),
         ]);
     }
 
@@ -28,7 +27,6 @@ class EventController extends AbstractController
     {
         $event = new Event();
         $form = $this->createForm(EventType::class, $event);
-
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -41,5 +39,23 @@ class EventController extends AbstractController
         return $this->render('event/new.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/fc-load-events', name: 'fc_load_events', methods: ['POST'])]
+    public function loadEvents(EventRepository $eventRepository): JsonResponse
+    {
+        $events = $eventRepository->findAll();
+        $eventsArray = [];
+
+        foreach ($events as $event) {
+            $eventsArray[] = [
+                'id' => $event->getId(),
+                'title' => $event->getName(),
+                'start' => $event->getDateBegin()->format('Y-m-d'),
+                'end' => $event->getDateEnd()->format('Y-m-d'),
+            ];
+        }
+
+        return new JsonResponse($eventsArray);
     }
 }
